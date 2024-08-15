5e7c0ef08f0092f4320f3aff6249980cf088be9a
Cathe1997 <cathe.gonzalez27997@gmail.com>
2024-08-15 23:20:45 +0200

    Agrego requisitos de longitud e igualdad

--- registro/js/validacion.js
+++ registro/js/validacion.js
@@ -5,3 +5,16 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
+
+//La contraseña debe tener máximo 6 dígitos.
+if (password1.lenght < 6) {
+    showAlertError();
+    return;
+}
+
+//Ambas contraseñas deben coincidir.
+if (password1 !== password2){
+    showAlertError();
+    return;
+}
+
